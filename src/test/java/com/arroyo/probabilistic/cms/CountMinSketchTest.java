32045c182442e3c15a33e8b3e5a4174944176219
Arroyo75 <118769522+Arroyo75@users.noreply.github.com>
2026-09-09 23:31:22 +0200

    test: basic tests for count min sketch

--- src/test/java/com/arroyo/probabilistic/cms/CountMinSketchTest.java
+++ src/test/java/com/arroyo/probabilistic/cms/CountMinSketchTest.java
@@ -3,7 +3,7 @@ package com.arroyo.probabilistic.cms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CountMinSketchTest {
 
@@ -15,10 +15,49 @@ public class CountMinSketchTest {
     }
 
     @Test
-    void insertedElementIsFound() {
+    void insertedElementCountIsOne() {
         cms.add("Palindrome");
         assertEquals(1, cms.minimalFrequency("Palindrome"));
     }
+
+    @Test
+    void multipleInsertedElementsAreFound() {
+        cms.add("Pneumatic");
+        cms.add("Hippopotamus");
+        cms.add("Flowery");
+
+        assertEquals(1, cms.minimalFrequency("Pneumatic"));
+        assertEquals(1, cms.minimalFrequency("Hippopotamus"));
+        assertEquals(1, cms.minimalFrequency("Flowery"));
+    }
+
+    @Test
+    void checkForNonexistentRecord() {
+        assertEquals(0, cms.minimalFrequency("Flowery"));
+    }
+
+    @Test
+    void insertedSingleLetterIsFound() {
+        cms.add("X");
+        assertEquals(1, cms.minimalFrequency("X"));
+    }
+
+    @Test
+    void insertedEmptyString() {
+        cms.add("");
+        assertEquals(1, cms.minimalFrequency(""));
+    }
+
+    @Test
+    void insertedNullHandled() {
+        assertThrows(NullPointerException.class, () -> cms.add(null));
+    }
+
+    @Test
+    void checkForNull() {
+        assertThrows(NullPointerException.class, () -> cms.minimalFrequency(null));
+    }
+
 
 
 }
